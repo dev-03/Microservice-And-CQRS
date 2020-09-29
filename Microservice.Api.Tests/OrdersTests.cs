2774c2ba1fbddad48d08d15451f36243b97117b7
dev <dev@linux-01.local>
2020-09-29 01:57:34 -0500

    rename Microservice.Api and added unit test project

---
+++ Microservice.Api.Tests/OrdersTests.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+using Xunit;
+using Microservice.Infrastructure;
+using Microservice.Api;
+using NSubstitute;
+
+namespace Microservice.Api.Tests
+{
+    public class OrdersTests
+    {
+        [Fact]
+        public void GetOrder()
+        {
+            Assert.NotNull("Test");
+        }
+    }
+}
